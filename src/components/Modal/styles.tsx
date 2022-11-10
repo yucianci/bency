d132dc09cb868fc1cdf0bb9d6b4b9129243c624e
Yuri Cianci <yucianci@gmail.com>
2022-11-10 02:31:11 -0300

    :heavy_plus_sign:	Feat: added Modal component

---
+++ src/components/Modal/styles.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background: #242424;
+`;
+
+export const ModalHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+  border-bottom: 1px solid #383838;
+  margin: 0 20px;
+
+  h1 {
+    font-weight: 500;
+    font-size: 1.7rem;
+  }
+
+`;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Header } from './styles';
+import React, { useState } from 'react';
+import {
+  Main, ModalBody, Navbar,
+} from './styles';
+import Modal from '../../components/Modal';
+import Button from '../../components/Button';
 
-const Home = () => (
-  <Header>
-    <h1>Hackathon</h1>
-    <span>ble</span>
-  </Header>
-);
+const Home = () => {
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+
+  return (
+    <Main>
+      <Navbar>
+        <h3>Clientes</h3>
+        <Button variant="outlined" onClick={() => setModalIsOpen(true)}>
+          Cadastrar
+        </Button>
+        <Modal
+          title="Registro de Cliente"
+          modalIsOpen={modalIsOpen}
+          setModalIsOpen={setModalIsOpen}
+        >
+          <ModalBody>Formulário</ModalBody>
+        </Modal>
+      </Navbar>
+    </Main>
+  );
+};
 
 export default Home;
